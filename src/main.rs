51cf9cf76a3fc8d35692594fb1d88b12a1a99b3a
lilhammer111 <wwwwwdemon@gmail.com>
2024-07-23 19:32:35 +0800

    test gtk

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use std::cell::Cell;
+use std::rc::Rc;
+
+use gtk::glib::clone;
+use gtk::{glib, Application, Button, Orientation};
+use gtk::{prelude::*, ApplicationWindow};
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(application: &Application) {
+    // Create two buttons
+    let button_increase = Button::builder()
+        .label("Increase")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let button_decrease = Button::builder()
+        .label("Decrease")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let number = Rc::new(Cell::new(0));
+    button_increase.connect_clicked(clone!(
+        #[weak]
+        number,
+        #[strong]
+        button_decrease,
+        move |_| {
+            number.set(number.get() + 1);
+            button_decrease.set_label(&number.get().to_string());
+        }
+    ));
+    button_decrease.connect_clicked(clone!(
+        // #[weak]
+        // number,
+        #[strong]
+        button_increase,
+        move |_| {
+            number.set(number.get() - 1);
+            button_increase.set_label(&number.get().to_string());
+        }
+    ));
+
+    let gtk_box = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+
+    gtk_box.append(&button_increase);
+    gtk_box.append(&button_decrease);
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(application)
+        .title("My GTK App")
+        .child(&gtk_box)
+        .build();
+
+    // Present the window
+    window.present();
+}
